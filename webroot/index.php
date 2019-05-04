b62388efd2971aab5b7156248d51808b2de33035
danishi <mail@example.com>
2019-05-04 13:55:54 +0900

    文字数カウンタと入力文字数制限を実装

--- webroot/index.php
+++ webroot/index.php
@@ -9,7 +9,6 @@ $post_text = str_replace(')', '', str_replace('(', '', $post_text));
 /*
 todo
 ・ログ実装
-・文字数制限（カウント付き）
 ・ツイートボタン
 ・見た目を整える
 */
@@ -35,8 +34,12 @@ todo
         <h1>不自然な日本語ジェネレーター</h1>
         <main>
             <form id="submit_area" action = "index.php" method = "post">
-                <p>変換したいテキストを入力してください。<p>
-                <textarea name="text" class="form-control" rows="3"><?= $post_text ?></textarea>
+                <p class="text-info">変換したいテキストを入力してください。<p>
+                <textarea name="text" class="form-control" rows="10" maxlength="1000"><?= $post_text ?></textarea>
+                <p class="text-muted">
+                    <span id="countor">0</span>/1000
+                </p>
+
                 <button type="submit" class="btn btn-default">変換</button>
             </form>
             <div>
@@ -44,4 +47,13 @@ todo
             </div>
         </main>
     </body>
+    <script>
+    $(function(){
+        $('#countor').html($('textarea').val().length);
+
+        $('textarea').bind('keydown keyup keypress change',function(){
+            $('#countor').html($(this).val().length);
+        });
+    });
+    </script>
 </html>
