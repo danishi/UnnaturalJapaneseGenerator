8595734e2f8cae4f8e17c40a673c28525041d3d8
danishi <mail@example.com>
2019-05-05 05:11:41 +0900

    完成

--- webroot/index.php
+++ webroot/index.php
@@ -5,12 +5,7 @@ use UnnaturalJapaneseGenerator\UnnaturalJapaneseGenerator;
 
 $post_text = filter_input(INPUT_POST, 'text', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $post_text = str_replace(')', '', str_replace('(', '', $post_text));
-
-/*
-todo
-・ツイートボタン
-・見た目を整える
-*/
+$ujg_text  = new UnnaturalJapaneseGenerator($post_text);
 
 ?>
 <!DOCTYPE>
@@ -24,7 +19,7 @@ todo
     <meta name=description content="">
     <meta name=keywords content="誤翻訳">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css?<?=time()?>>">
+    <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
@@ -43,22 +38,17 @@ todo
     <h1></h1>
     <main class="container">
         <form id="submit_area" action="index.php" method="post">
-            <div class="jumbotron">
-                <h4>無理矢理日本語変換したような不自然な日本語を生成します。</h4>
-            </div>
             <div class="form-group">
-                <p class="text-info">変換したいテキストを入力してください。<p>
-                <textarea name="text" class="form-control" rows="10" maxlength="1000"><?= $post_text ?></textarea>
-                <p class="text-muted">
+                <p class="text-info">日本語を中国語に翻訳してから再度日本語に翻訳しなおすことで、翻訳ミスしたような不自然な日本語を生成します。
+                <br>Twitterに投稿してフォロワーをざわつかせたり、自由にお使いください。<p>
+
+                <textarea name="text" class="form-control" rows="10" maxlength="1000" placeholder="変換したいテキストを入力してください。"><?= $post_text ?></textarea>
+                <div id="countor_area" class="text-muted">
                     <span id="countor">0</span>/1000
-                </p>
+                </div>
             </div>
+
             <button type="submit" class="btn btn-success"><i class="fas fa-sync-alt"></i> 変換</button>
-            <a class="twitter-share-button"
-                href="https://twitter.com/intent/tweet?text=Hello%20world"
-                data-size="large">
-            ツイートする</a>
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </form>
 
         <div class="card">
@@ -66,15 +56,21 @@ todo
                 変換後テキスト
             </div>
             <div class="card-body">
-                <?= new UnnaturalJapaneseGenerator($post_text) ?>
+                <?= $ujg_text ?>
             </div>
-            <button id="select_btn" class="btn btn-info"><i class="far fa-clipboard"></i> 選択</button>
+            <button id="select_btn" class="btn( btn-info"><i class="far fa-clipboard"></i> 選択</button>
         </div>
+        <?php if(!empty($post_text)) : ?>
+        <hr>
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw&text=<?= urlencode($ujg_text) ?>"
+         class="twitter-share-button" data-show-count="false" data-size="large">ツイート</a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <?php endif; ?>
 
     </main>
     <footer class="footer">
         <div class="container">
-            <p class="text-muted">copyright <a target="_blank" href="https://twitter.com/Danishi411">danishi</a></p>
+            <p class="text-muted">Created by <a target="_blank" href="https://twitter.com/Danishi411">Danishi</a>.</p>
         </div>
     </footer>
 </body>
