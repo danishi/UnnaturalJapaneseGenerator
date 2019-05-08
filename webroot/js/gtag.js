6174c55b92d044e6bf4db91597ec2a5893bd5843
danishi <mail@example.com>
2019-05-09 07:33:43 +0900

    gtag.jsの修正

--- webroot/js/gtag.js
+++ webroot/js/gtag.js
@@ -1,3 +1,3 @@
 document.write('<!-- Global site tag (gtag.js) - Google Analytics -->');
 document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-137472393-2"></script>');
-document.write('<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-137472393-2');</script')');
+document.write("<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-137472393-2');</script>");
